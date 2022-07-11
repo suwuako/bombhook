db04704b6bc8158e9b53af7d53766024ce7d9026
unknown <ronnielow@protonmail.com>
2022-07-12 00:56:59 +0800

    woa it works

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import asyncio
 
 import src.tui as tui
+import src.game_elements as game
 
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
@@ -18,17 +19,27 @@ class start():
 
         if platform in windows:
             driver = fr"{path}\external\geckodriver.exe"
+            wordlist = open(f"{path}\external\wordlist", "r")
         elif platform in linux:
             driver = fr"{path}.external.geckodriver"
+            wordlist = open(f"{path}\external\wordlist", "r")
         else:
             driver = fr"{path}.external.geckodriver"
+            wordlist = open(f"{path}\external\wordlist", "r")
 
         self.driver = webdriver.Firefox(service=Service(driver))
+        self.wordlist = []
+
+        lines = wordlist.readlines()
+        for i in lines:
+            self.wordlist.append(i.replace("\n", ""))
+
+        del(wordlist)
 
     def run(self):
         tui.load_room(self.driver)
         tui.in_game(self.driver)
-        tui.playing_game(self.driver)
+        game.play(self.driver, self.wordlist)
 
 
 if __name__ == "__main__":
